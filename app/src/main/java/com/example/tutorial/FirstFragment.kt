94886614b35c1a9723bc7fd582fa5851a4903163
James Gao <jamesg@gatech.edu>
2024-02-27 20:27:39 -0500

    working queryUsageStats

--- app/src/main/java/com/example/tutorial/FirstFragment.kt
+++ app/src/main/java/com/example/tutorial/FirstFragment.kt
@@ -1,15 +1,23 @@
 package com.example.tutorial
 
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.tutorial.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -34,6 +42,28 @@ private var _binding: FragmentFirstBinding? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+            var usageStatsManager =
+        requireContext().getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+
+        val cal = Calendar.getInstance()
+        val currentTime = cal.timeInMillis
+        cal.add(Calendar.YEAR, -2) // Set year to beginning of desired period.
+        val beginTime = cal.timeInMillis // Get begin time in milliseconds
+
+        val queryUsageStats = usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_YEARLY,
+            beginTime,
+            currentTime
+        )
+        Log.d("TAG", queryUsageStats.toString());
+        for (element in queryUsageStats) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                Log.d("name", element.packageName);
+                Log.d("time", element.totalTimeVisible.toString())
+            };
+        }
+
         binding.randomButton.setOnClickListener {
 //            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
             val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
@@ -43,14 +73,10 @@ private var _binding: FragmentFirstBinding? = null
         }
 
 //        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
-//            // create a Toast with some text, to appear for a short time
-//            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
-//            // show the Toast
-//            myToast.show()
 //        }
         binding.toastButton.setOnClickListener {
             // create a Toast with some text, to appear for a short time
-            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
+            val myToast = Toast.makeText(context, queryUsageStats.toString(), Toast.LENGTH_SHORT)
             // show the Toast
             myToast.show()
         }
